659bff1c8d6be3f4dcc45d8d5fca7fd4c0a5a2db
jygonzalez <jorge.gonzalezalcoce@ucalgary.ca>
2024-05-09 08:42:21 -0600

    Submit assignment1: Delete file to test repo, add 2 screenshots, add updated README and code

---
+++ assignment1code.py
@@ -0,0 +1,12 @@
+# ENSF 692 Spring 2024
+# Assignment 1 Starter Code
+# Yael Gonzalez
+
+studentName = "Yael Gonzalez"  # Change the contents of this variable to your name (leave the quotation marks).
+bestSkill = "playing padel tennis"
+
+print("Hello, World!")
+print("My name is " + studentName + " and I am a student in ENSF 692.")
+
+# print your best skill here
+print("My best skill is " + bestSkill)
